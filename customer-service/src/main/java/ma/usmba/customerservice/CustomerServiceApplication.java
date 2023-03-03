ff721e7980ac594a571351295a3eb335260d4099
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-03 19:59:45 +0100

    application

--- customer-service/src/main/java/ma/usmba/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/usmba/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,32 @@
 package ma.usmba.customerservice;
 
+import ma.usmba.customerservice.config.CustomerConfigParams;
+import ma.usmba.customerservice.entities.Customer;
+import ma.usmba.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
+    @Bean
+    public CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            for (int i = 1; i <= 5 ; i++) {
+                Customer customer=Customer.builder()
+                        .firstName("First Name "+i)
+                        .lastName("Last Name "+i)
+                        .email("f.l"+i+"@gmail.com")
+                        .build();
+                customerRepository.save(customer);
+            }
+        };
+    }
 }
